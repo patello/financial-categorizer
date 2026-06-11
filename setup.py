3d22adcca5553460db9cb2b2385a1d7502dd1791
Patrik Ekenberg <patrik@ekenberg.com>
2026-06-11 22:41:42 +0200

    feat: expose global entrypoint console script 'financial-categorizer'

--- setup.py
+++ setup.py
@@ -3,4 +3,10 @@ from setuptools import setup
 setup(
     name="financial-categorizer",
     packages=["financial_categorizer"],
+    py_modules=["cli"],
+    entry_points={
+        "console_scripts": [
+            "financial-categorizer=cli:main",
+        ],
+    },
 )
